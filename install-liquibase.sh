0bd4cc2e776815e9df7ade1fdf8a5e3ac938bef8
hexqueller <dima.nesed@gmail.com>
2024-10-06 13:05:34 +0300

    add func

--- install-liquibase.sh
+++ install-liquibase.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+DB_NAME=postgres
+SQL=$(cat <<EOF
+INSERT INTO students (name, created_at, "group") VALUES
+('John Doe', '2023-10-01 10:00:00', 'Group A'),
+('Jane Smith', '2023-10-02 11:00:00', 'Group B'),
+('Alice Johnson', '2023-10-03 12:00:00', 'Group C'),
+('Bob Brown', '2023-10-04 13:00:00', 'Group A');
+EOF
+)
 
 if [ "$(cat /etc/hostname)" == "pg01" ]; then
     # Установка
@@ -10,4 +19,7 @@ if [ "$(cat /etc/hostname)" == "pg01" ]; then
     git clone https://github.com/hexqueller/ipr-postgresql.git
     cd ipr-postgresql
     liquibase update
+
+    # Заполнение данными
+    sudo -u vagrant env DB_NAME="$DB_NAME" SQL="$SQL" bash -c 'source /home/vagrant/.bashrc && /usr/local/pgsql/bin/psql -d "$DB_NAME" -c "$SQL"'
 fi
