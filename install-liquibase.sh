02a287dbc90719312728b06cc3df13252419a09f
hexqueller <dima.nesed@gmail.com>
2024-10-06 12:49:43 +0300

    fix

--- install-liquibase.sh
+++ install-liquibase.sh
@@ -4,7 +4,7 @@ if [ "$(cat /etc/hostname)" == "pg01" ]; then
     # Установка
     rpm --import https://repo.liquibase.com/liquibase.asc
     yum-config-manager --add-repo https://repo.liquibase.com/repo-liquibase-com.repo
-    yum install liquibase -y
+    yum install java-21-openjdk liquibase -y
 
     # Проливка
     git clone https://github.com/hexqueller/ipr-postgresql.git
